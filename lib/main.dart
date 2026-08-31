e142f04267c055ad621e74e15b83d8f1fca8b35c
Badhon100 <badhonx102@gmail.com>
2026-08-31 16:48:17 +0600

    chore: add dependencies and manifest permissions for background playback

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/widgets.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 import 'app/app.dart';
 import 'app/dependencies.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.audioservice.channel.audio',
+    androidNotificationChannelName: 'Audio Playback',
+    androidNotificationOngoing: true,
+  );
   runApp(OneBeatApp(dependencies: AppDependencies.create()));
 }
